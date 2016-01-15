3f4e2d1c684d036f1082a450a6b45ddaf2f50e23
Oliver Loftus <o@oloft.us>
2016-01-15 15:16:42 +0000

    Added Dtxy component

---
+++ project_1/project_1.srcs/sources_1/imports/new/Dtxy.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Dtxy is
+    generic (
+        precision : INTEGER
+    );
+    port (
+        Z : in STD_LOGIC_VECTOR (3 downto 0);
+        X : in UNSIGNED (precision - 1 downto 0);
+        Y : out UNSIGNED (precision - 1 downto 0)
+    );
+end Dtxy;
+
+architecture Behavioral of Dtxy is
+begin
+    process (X, Z) begin
+        case Z is
+            when "1000" => -- Z4
+                Y <= one;
+        
+            when "0100" => -- Z3
+                Y <= Shift_Right(X, 2) + ("0"&"110110");
+                
+            when "0010" => -- Z2
+                Y <= X + ("0"&"101000");
+                
+            when "0001" => -- Z1
+                Y <= Shift_Left(X, 1) + ("0"&"100000");
+                
+            when others =>
+                null;
+        end case;
+    end process;
+
+end Behavioral;
