d34d43ee7552a3bafe71e9221f4a43949c56e3ec
Oliver Loftus <o@oloft.us>
2016-01-19 23:01:42 +0000

    Add sign bit output to Complements2To1

--- project_1/project_1.srcs/sim_1/new/Complements2To1TB.vhd
+++ project_1/project_1.srcs/sim_1/new/Complements2To1TB.vhd
@@ -15,12 +15,14 @@ architecture Behavioral of Complements2To1TB is
         );
         port (
             twos : in SIGNED (precision - 1 downto 0);
-            ones : out UNSIGNED (precision - 1 downto 0)
+            ones : out UNSIGNED (precision - 1 downto 0);
+            sign : out STD_LOGIC
         );
     end component;
     
     signal sigTwos : SIGNED (tbPrecision - 1 downto 0);
     signal sigOnes : UNSIGNED (tbPrecision - 1 downto 0);
+    signal sigSign : STD_LOGIC;
 begin
     uut: Complements2To1
         generic map (
@@ -28,25 +30,30 @@ begin
         )
         port map (
             twos => sigTwos,
-            ones => sigOnes
+            ones => sigOnes,
+            sign => sigSign
         );
  
     tb: process begin
         sigTwos <= "1000"&"0010"; -- -126
         wait for 100 ns;
         assert sigOnes = "0111"&"1110" report "Test failed: 2's complement 1000 0010 (-126) should be 0111 1110 (126)";
+        assert sigSign = '1' report "Test failed: Input -126 should output sign 1";
 
         sigTwos <= "1111"&"1111"; -- -1
         wait for 100 ns;
         assert sigOnes = "0000"&"0001" report "Test failed: 2's complement 1111 1111 (-1) should be 0000 0001 (1)";
+        assert sigSign = '1' report "Test failed: Input -1 should output sign 1";
 
         sigTwos <= "0111"&"1110";
         wait for 100 ns;
         assert sigOnes = "0111"&"1110" report "Test failed: 2's complement 0111 1110 (126) should be 0111 1110 (126)";
+        assert sigSign = '0' report "Test failed: Input 126 should output sign 0";
         
         sigTwos <= "0000"&"0001";
         wait for 100 ns;
         assert sigOnes = "0000"&"0001" report "Test failed: 2's complement 0000 0001 (1) should be 0000 0001 (1)";
+        assert sigSign = '0' report "Test failed: Input 1 should output sign 0";
     end process;
     
 end Behavioral;
--- project_1/project_1.srcs/sources_1/new/Complements2To1.vhd
+++ project_1/project_1.srcs/sources_1/new/Complements2To1.vhd
@@ -8,13 +8,17 @@ entity Complements2To1 is
     );
     port (
         twos : in SIGNED (precision - 1 downto 0);
-        ones : out UNSIGNED (precision - 1 downto 0)
+        ones : out UNSIGNED (precision - 1 downto 0);
+        sign : out STD_LOGIC -- sign = 1 means negative
     );
 end Complements2To1;
 
 architecture Behavioral of Complements2To1 is
     signal twosUnsigned : UNSIGNED (precision - 1 downto 0);
+    signal sigSign : STD_LOGIC;
 begin
     twosUnsigned <= UNSIGNED(STD_LOGIC_VECTOR(twos));
-    ones <= 2 ** (precision) - twosUnsigned when twos(precision - 1) = '1' else twosUnsigned;
+    sigSign <= '1' when twos(precision - 1) = '1' else '0';
+    sign <= sigSign;
+    ones <= 2 ** (precision) - twosUnsigned when sigSign = '1' else twosUnsigned;
 end Behavioral;
