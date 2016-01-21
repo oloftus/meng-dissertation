bb5a471582dcb4fa0b991b25cd6c9ef4e5df9015
Oliver Loftus <o@oloft.us>
2016-01-21 18:35:24 +0000

    Complements2To1 now has longer signed input and clearer support for decimal numbers

--- project_1/project_1.srcs/sim_1/new/Complements2To1TB.vhd
+++ project_1/project_1.srcs/sim_1/new/Complements2To1TB.vhd
@@ -14,13 +14,13 @@ architecture Behavioral of Complements2To1TB is
             precision : INTEGER
         );
         port (
-            twos : in SIGNED (precision - 1 downto 0);
+            twos : in SIGNED (precision downto 0);
             ones : out UNSIGNED (precision - 1 downto 0);
             sign : out STD_LOGIC
         );
     end component;
     
-    signal sigTwos : SIGNED (tbPrecision - 1 downto 0);
+    signal sigTwos : SIGNED (tbPrecision downto 0);
     signal sigOnes : UNSIGNED (tbPrecision - 1 downto 0);
     signal sigSign : STD_LOGIC;
 begin
@@ -35,24 +35,34 @@ begin
         );
  
     tb: process begin
-        sigTwos <= "1000"&"0010"; -- -126
-        wait for 100 ns;
-        assert sigOnes = "0111"&"1110" report "Test failed: 2's complement 1000 0010 (-126) should be 0111 1110 (126)";
-        assert sigSign = '1' report "Test failed: Input -126 should output sign 1";
+        sigTwos <= "110111"&"001";
+        wait for 100ns;
+        assert sigOnes = "01000"&"111" report "Test failed: 2's complement 110111 001 (-8.875) should be 1000 111 (8.875)";
+        assert sigSign = '1' report "Test failed: Input -8.875 should output sign 1";
 
-        sigTwos <= "1111"&"1111"; -- -1
+        sigTwos <= "100001"&"000";
         wait for 100 ns;
-        assert sigOnes = "0000"&"0001" report "Test failed: 2's complement 1111 1111 (-1) should be 0000 0001 (1)";
+        assert sigOnes = "11111"&"000" report "Test failed: 2's complement 100001 000 (-32) should be 11111 000 (32)";
+        assert sigSign = '1' report "Test failed: Input -32 should output sign 1";
+
+        sigTwos <= "111111"&"000";
+        wait for 100 ns;
+        assert sigOnes = "00001"&"000" report "Test failed: 2's complement 111111 000 (-1) should be 00001 000 (1)";
         assert sigSign = '1' report "Test failed: Input -1 should output sign 1";
 
-        sigTwos <= "0111"&"1110";
-        wait for 100 ns;
-        assert sigOnes = "0111"&"1110" report "Test failed: 2's complement 0111 1110 (126) should be 0111 1110 (126)";
-        assert sigSign = '0' report "Test failed: Input 126 should output sign 0";
+        sigTwos <= "001000"&"111";
+        wait for 100ns;
+        assert sigOnes = "01000"&"111" report "Test failed: 2's complement 01000 111 (8.875) should be 1000 111 (8.875)";
+        assert sigSign = '0' report "Test failed: Input -8.875 should output sign 1";
         
-        sigTwos <= "0000"&"0001";
+        sigTwos <= "011111"&"000";
         wait for 100 ns;
-        assert sigOnes = "0000"&"0001" report "Test failed: 2's complement 0000 0001 (1) should be 0000 0001 (1)";
+        assert sigOnes = "11111"&"000" report "Test failed: 2's complement 0111 1110 (32) should be 0111 1110 (32)";
+        assert sigSign = '0' report "Test failed: Input 32 should output sign 0";
+        
+        sigTwos <= "000001"&"000";
+        wait for 100 ns;
+        assert sigOnes = "00001"&"000" report "Test failed: 2's complement 0000 0001 (1) should be 0000 0001 (1)";
         assert sigSign = '0' report "Test failed: Input 1 should output sign 0";
     end process;
     
--- project_1/project_1.srcs/sources_1/new/Complements2To1.vhd
+++ project_1/project_1.srcs/sources_1/new/Complements2To1.vhd
@@ -7,18 +7,23 @@ entity Complements2To1 is
         precision : INTEGER
     );
     port (
-        twos : in SIGNED (precision - 1 downto 0);
+        twos : in SIGNED (precision downto 0); -- Extra bit for sign bit
         ones : out UNSIGNED (precision - 1 downto 0);
         sign : out STD_LOGIC -- sign = 1 means negative
     );
 end Complements2To1;
 
 architecture Behavioral of Complements2To1 is
-    signal twosUnsigned : UNSIGNED (precision - 1 downto 0);
+    constant max : UNSIGNED (precision downto 0) := (others => '1');
+
+    signal twosUnsigned : UNSIGNED (precision downto 0);
+    signal onesLong : UNSIGNED (precision downto 0);
     signal sigSign : STD_LOGIC;
 begin
-    twosUnsigned <= UNSIGNED(STD_LOGIC_VECTOR(twos));
-    sigSign <= '1' when twos(precision - 1) = '1' else '0';
+    sigSign <= '1' when twos(precision) = '1' else '0';
     sign <= sigSign;
-    ones <= 2 ** (precision) - twosUnsigned when sigSign = '1' else twosUnsigned;
+
+    twosUnsigned <= UNSIGNED(STD_LOGIC_VECTOR(twos));
+    onesLong <= max - twosUnsigned + 1 when sigSign = '1' else twosUnsigned;
+    ones <= onesLong(precision - 1 downto 0);
 end Behavioral;
