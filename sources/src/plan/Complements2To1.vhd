daafd1a9fbe9f7b76dbc2de0bda93176668c8ff2
Oliver Loftus <o@oloft.us>
2016-01-24 19:48:32 +0000

    Minor changes

--- sources/src/plan/Complements2To1.vhd
+++ sources/src/plan/Complements2To1.vhd
@@ -17,13 +17,12 @@ architecture Behavioral of Complements2To1 is
     constant max : UNSIGNED (precision downto 0) := (others => '1');
 
     signal twosUnsigned : UNSIGNED (precision downto 0);
-    signal onesLong : UNSIGNED (precision downto 0);
+    signal onesBig : UNSIGNED (precision downto 0);
     signal sigSign : STD_LOGIC;
 begin
     sigSign <= '1' when twos(precision) = '1' else '0';
     sign <= sigSign;
-
     twosUnsigned <= UNSIGNED(STD_LOGIC_VECTOR(twos));
-    onesLong <= max - twosUnsigned + 1 when sigSign = '1' else twosUnsigned;
-    ones <= onesLong(precision - 1 downto 0);
+    onesBig <= max - twosUnsigned + 1 when sigSign = '1' else twosUnsigned;
+    ones <= onesBig(precision - 1 downto 0);
 end Behavioral;
