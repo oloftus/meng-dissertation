f0592064a9d30aac483fa8803aacc3f6a1a0cd9f
Oliver Loftus <o@oloft.us>
2016-02-06 23:00:06 +0000

    Rewrote Synapse that now synthesises

--- sources/test/synapse/SynapseTB.vhd
+++ sources/test/synapse/SynapseTB.vhd
@@ -3,39 +3,35 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SynapseTB is
     generic (
-        tbSize : INTEGER := 4
+        size : INTEGER := 4
     );
 end SynapseTB;
 
 architecture Behavioral of SynapseTB is
-    signal sigClk, sigRst, sigClr, sigSynInValid, sigSynOutValid : STD_LOGIC;
-    signal sigSynIn, sigSynOut : STD_LOGIC_VECTOR (tbSize - 1 downto 0);
+    signal sigClk, sigClr, sigRst, sigSynInValid, sigSynOutValid : STD_LOGIC;
+    signal sigSynIn, sigSynOut : STD_LOGIC_VECTOR (size - 1 downto 0);
 
     component Synapse is
-        generic (
-            size : INTEGER
-        );
+--        generic (
+--            size : INTEGER
+--        );
         port (
-            CLK : in STD_LOGIC;
-            CLR : in STD_LOGIC;
-            RST : in STD_LOGIC;
-            
+            CLK, CLR, RST : in STD_LOGIC;
             SYN_IN_VALID : in STD_LOGIC;
             SYN_IN : in STD_LOGIC_VECTOR (size - 1 downto 0);
-            
             SYN_OUT_VALID : out STD_LOGIC;
             SYN_OUT : out STD_LOGIC_VECTOR (size - 1 downto 0)
         );
     end component;
 begin
     uut: Synapse
-        generic map (
-            size => tbSize
-        )
+--        generic map (
+--            size => size
+--        )
         port map (
             CLK => sigClk,
-            RST => sigRst,
             CLR => sigClr,
+            RST => sigRst,
             SYN_IN_VALID => sigSynInValid,
             SYN_IN => sigSynIn,
             SYN_OUT_VALID => sigSynOutValid,
@@ -54,6 +50,9 @@ begin
         constant three : STD_LOGIC_VECTOR (3 downto 0) := "0011";
         constant twelve : STD_LOGIC_VECTOR (3 downto 0) := "1100";
     begin
+        sigClr <= '0';
+        sigSynInValid <= '0';
+        
         sigRst <= '1';
         wait for 200ns;
         sigRst <= '0';
