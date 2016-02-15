78c02e6fba2cf501b96f3bfada87b27f37ed0a36
Oliver Loftus <o@oloft.us>
2016-02-15 11:16:11 +0000

    Added ValueRouter and Or2 components (without tests)

---
+++ sources/src/value_router/xgui/ValueRouter_v1_0.tcl
@@ -0,0 +1,52 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+	set Page0 [ipgui::add_page $IPINST -name "Page 0" -layout vertical]
+	set Component_Name [ipgui::add_param $IPINST -parent $Page0 -name Component_Name]
+	set address [ipgui::add_param $IPINST -parent $Page0 -name address]
+	set packetOutWidth [ipgui::add_param $IPINST -parent $Page0 -name packetOutWidth]
+	set packetInWidth [ipgui::add_param $IPINST -parent $Page0 -name packetInWidth]
+}
+
+proc update_PARAM_VALUE.address { PARAM_VALUE.address } {
+	# Procedure called to update address when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.address { PARAM_VALUE.address } {
+	# Procedure called to validate address
+	return true
+}
+
+proc update_PARAM_VALUE.packetOutWidth { PARAM_VALUE.packetOutWidth } {
+	# Procedure called to update packetOutWidth when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.packetOutWidth { PARAM_VALUE.packetOutWidth } {
+	# Procedure called to validate packetOutWidth
+	return true
+}
+
+proc update_PARAM_VALUE.packetInWidth { PARAM_VALUE.packetInWidth } {
+	# Procedure called to update packetInWidth when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.packetInWidth { PARAM_VALUE.packetInWidth } {
+	# Procedure called to validate packetInWidth
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.packetInWidth { MODELPARAM_VALUE.packetInWidth PARAM_VALUE.packetInWidth } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.packetInWidth}] ${MODELPARAM_VALUE.packetInWidth}
+}
+
+proc update_MODELPARAM_VALUE.packetOutWidth { MODELPARAM_VALUE.packetOutWidth PARAM_VALUE.packetOutWidth } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.packetOutWidth}] ${MODELPARAM_VALUE.packetOutWidth}
+}
+
+proc update_MODELPARAM_VALUE.address { MODELPARAM_VALUE.address PARAM_VALUE.address } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.address}] ${MODELPARAM_VALUE.address}
+}
+
