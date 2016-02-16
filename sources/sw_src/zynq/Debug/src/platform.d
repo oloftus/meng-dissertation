7347e7513177c48c5012eb97e3063addd9bd600c
Oliver Loftus <o@oloft.us>
2016-02-16 12:07:23 +0000

    Added "hello world" Zynq project

---
+++ sources/sw_src/zynq/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.d: ../src/platform.c \
+ C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xparameters.h \
+ C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+C:/Users/Administrator/Xilinx/xsdk_workspace/ann_pc_if_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
