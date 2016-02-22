e65439131b3bcc113d2610a30a7188b70dc50c10
Oliver Loftus <o@oloft.us>
2016-02-22 15:36:39 +0000

    Added for loops to parameterized script (not yet extracted variables)

---
+++ sources/scripts/create_network.pl
@@ -0,0 +1,207 @@
+use strict;
+use warnings;
+
+##########################
+
+my $NUM_LAYERS = 2;
+my $NEURONS_PER_LAYER = 2;
+
+##########################
+
+open my $fh, ">", "script.tcl";
+
+print $fh <<CMD;
+
+create_bd_port -dir I -type clk CLK
+create_bd_port -dir I -type rst RST
+create_bd_port -dir I -from 31 -to 0 -type data PKT_IN
+create_bd_port -dir I PKT_IN_VALID
+create_bd_port -dir O DONE_OUT
+
+CMD
+
+foreach my $id (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+create_bd_port -dir O -from 12 -to 0 -type data SYN_${id}_OUT
+create_bd_port -dir O SYN_${id}_OUT_VALID
+
+CMD
+}
+
+print $fh <<CMD;
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 TypeRouter_Stimulus
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 TypeRouter_Stimulus_Address
+create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 TypeRouter_Weight
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 TypeRouter_Weight_Address
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut_Layers
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_Layers
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut_StimulusRegisters
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_StimulusRegisters
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat
+
+CMD
+
+foreach my $lid (0..$NUM_LAYERS - 1) {
+print $fh <<CMD;
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 LayerRouter_${lid}
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 LayerRouter_${lid}_Address
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut_Layer_${lid}
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_Layer_${lid}
+
+CMD
+
+foreach my $nid (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:Neuron_wrapper:1.0 Neuron_${lid}_${nid}
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 Neuron_${lid}_${nid}_Address
+
+CMD
+}}
+
+foreach my $id (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 StimulusRegister_${id}
+
+CMD
+}
+
+print $fh <<CMD;
+
+set_property -dict [list CONFIG.packetOutWidth {31} CONFIG.packetInWidth {32}] [get_bd_cells TypeRouter_Stimulus]
+set_property -dict [list CONFIG.CONST_VAL {1}] [get_bd_cells TypeRouter_Stimulus_Address]
+set_property -dict [list CONFIG.packetOutWidth {31} CONFIG.packetInWidth {32}] [get_bd_cells TypeRouter_Weight]
+set_property -dict [list CONFIG.CONST_VAL {0}] [get_bd_cells TypeRouter_Weight_Address]
+set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_Layers]
+set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_StimulusRegisters]
+set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut]
+
+CMD
+
+foreach my $lid (0..$NUM_LAYERS - 1) {
+print $fh <<CMD;
+
+set_property -dict [list CONFIG.packetOutWidth {26} CONFIG.packetInWidth {31}] [get_bd_cells LayerRouter_${lid}]
+set_property -dict [list CONFIG.CONST_WIDTH {5} CONFIG.CONST_VAL {${lid}}] [get_bd_cells LayerRouter_${lid}_Address]
+set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_Layer_${lid}]
+
+CMD
+
+foreach my $nid (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {${nid}}] [get_bd_cells Neuron_${lid}_${nid}_Address]
+
+CMD
+}}
+
+
+foreach my $id (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+set_property -dict [list CONFIG.address {${id}} CONFIG.addressWidth {19} CONFIG.dataWidth {13}] [get_bd_cells StimulusRegister_${id}]
+
+CMD
+}
+
+print $fh <<CMD;
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins TypeRouter_Weight/CLK]
+connect_bd_net [get_bd_ports PKT_IN_VALID] [get_bd_pins TypeRouter_Weight/PKT_IN_VALID]
+connect_bd_net [get_bd_ports PKT_IN] [get_bd_pins TypeRouter_Weight/PKT_IN]
+connect_bd_net [get_bd_pins TypeRouter_Weight/DONE_IN] [get_bd_pins DoneOut_Layers/DOUT]
+connect_bd_net [get_bd_pins TypeRouter_Weight/DONE_OUT] [get_bd_pins DoneOutConcat/In0]
+connect_bd_net [get_bd_pins TypeRouter_Weight_Address/dout] [get_bd_pins TypeRouter_Weight/ADDR]
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins TypeRouter_Stimulus/CLK]
+connect_bd_net [get_bd_ports PKT_IN_VALID] [get_bd_pins TypeRouter_Stimulus/PKT_IN_VALID]
+connect_bd_net [get_bd_ports PKT_IN] [get_bd_pins TypeRouter_Stimulus/PKT_IN]
+connect_bd_net [get_bd_pins TypeRouter_Stimulus/DONE_IN] [get_bd_pins DoneOut_StimulusRegisters/DOUT]
+connect_bd_net [get_bd_pins TypeRouter_Stimulus/DONE_OUT] [get_bd_pins DoneOutConcat/In1]
+connect_bd_net [get_bd_pins TypeRouter_Stimulus_Address/dout] [get_bd_pins TypeRouter_Stimulus/ADDR]
+
+connect_bd_net [get_bd_ports DONE_OUT] [get_bd_pins DoneOut/DOUT]
+connect_bd_net [get_bd_pins DoneOutConcat/dout] [get_bd_pins DoneOut/DIN]
+connect_bd_net [get_bd_pins DoneOutConcat_Layers/dout] [get_bd_pins DoneOut_Layers/DIN]
+connect_bd_net [get_bd_pins DoneOutConcat_StimulusRegisters/dout] [get_bd_pins DoneOut_StimulusRegisters/DIN]
+
+CMD
+
+foreach my $id (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins StimulusRegister_${id}/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins StimulusRegister_${id}/RST]
+connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT] [get_bd_pins StimulusRegister_${id}/PKT_IN]
+connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT_VALID] [get_bd_pins StimulusRegister_${id}/PKT_IN_VALID]
+connect_bd_net [get_bd_pins StimulusRegister_${id}/DONE_OUT] [get_bd_pins DoneOutConcat_StimulusRegisters/In${id}]
+
+CMD
+}
+
+foreach my $lid (0..$NUM_LAYERS - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT_VALID] [get_bd_pins LayerRouter_${lid}/PKT_IN_VALID]
+connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT] [get_bd_pins LayerRouter_${lid}/PKT_IN]
+connect_bd_net [get_bd_ports CLK] [get_bd_pins LayerRouter_${lid}/CLK]
+connect_bd_net [get_bd_pins DoneOutConcat_Layer_${lid}/dout] [get_bd_pins DoneOut_Layer_${lid}/DIN]
+connect_bd_net [get_bd_pins DoneOut_Layer_${lid}/DOUT] [get_bd_pins LayerRouter_${lid}/DONE_IN]
+connect_bd_net [get_bd_pins LayerRouter_${lid}_Address/dout] [get_bd_pins LayerRouter_${lid}/ADDR]
+connect_bd_net [get_bd_pins LayerRouter_${lid}/DONE_OUT] [get_bd_pins DoneOutConcat_Layers/In${lid}]
+
+CMD
+
+foreach my $nid (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins Neuron_${lid}_${nid}/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins Neuron_${lid}_${nid}/RST]
+connect_bd_net [get_bd_pins Neuron_${lid}_${nid}_Address/dout] [get_bd_pins Neuron_${lid}_${nid}/NEURON_ADDR]
+connect_bd_net [get_bd_pins LayerRouter_${lid}/PKT_OUT] [get_bd_pins Neuron_${lid}_${nid}/PKT_IN]
+connect_bd_net [get_bd_pins LayerRouter_${lid}/PKT_OUT_VALID] [get_bd_pins Neuron_${lid}_${nid}/PKT_IN_VALID]
+connect_bd_net [get_bd_pins Neuron_${lid}_${nid}/DONE_OUT] [get_bd_pins DoneOutConcat_Layer_${lid}/In${nid}]
+
+CMD
+}}
+
+foreach my $nid1 (0..$NEURONS_PER_LAYER - 1) {
+foreach my $nid2 (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_pins StimulusRegister_${nid1}/VAL_OUT] [get_bd_pins Neuron_0_${nid2}/SYN_${nid1}_DIN]
+connect_bd_net [get_bd_pins StimulusRegister_${nid1}/VAL_OUT_VALID] [get_bd_pins Neuron_0_${nid2}/SYN_${nid1}_VALID]
+
+CMD
+}}
+
+foreach my $lid (0..$NUM_LAYERS - 2) {
+foreach my $nid1 (0..$NEURONS_PER_LAYER - 1) {
+foreach my $nid2 (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_pins Neuron_${lid}_${nid1}/SYN_OUT] [get_bd_pins Neuron_@{[$lid + 1]}_${nid2}/SYN_${nid1}_DIN]
+connect_bd_net [get_bd_pins Neuron_${lid}_${nid1}/SYN_OUT_VALID] [get_bd_pins Neuron_@{[$lid + 1]}_${nid2}/SYN_${nid1}_VALID]
+
+CMD
+}}}
+
+foreach my $nid (0..$NEURONS_PER_LAYER - 1) {
+print $fh <<CMD;
+
+connect_bd_net [get_bd_pins Neuron_@{[$NUM_LAYERS - 1]}_${nid}/SYN_OUT] [get_bd_ports SYN_${nid}_OUT]
+connect_bd_net [get_bd_pins Neuron_@{[$NUM_LAYERS - 1]}_${nid}/SYN_OUT_VALID] [get_bd_ports SYN_${nid}_OUT_VALID]
+
+CMD
+}
+
+print $fh <<CMD;
+
+regenerate_bd_layout
+
+CMD
--- sources/scripts/script.tcl
+++ sources/scripts/script.tcl
@@ -1,18 +1,19 @@
-##### Create ports #####
+
 create_bd_port -dir I -type clk CLK
 create_bd_port -dir I -type rst RST
 create_bd_port -dir I -from 31 -to 0 -type data PKT_IN
 create_bd_port -dir I PKT_IN_VALID
 create_bd_port -dir O DONE_OUT
 
-# Configurable
+
 create_bd_port -dir O -from 12 -to 0 -type data SYN_0_OUT
 create_bd_port -dir O SYN_0_OUT_VALID
+
 
 create_bd_port -dir O -from 12 -to 0 -type data SYN_1_OUT
 create_bd_port -dir O SYN_1_OUT_VALID
 
-##### Create components #####
+
 create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 TypeRouter_Stimulus
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 TypeRouter_Stimulus_Address
 create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 TypeRouter_Weight
@@ -24,33 +25,41 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_StimulusR
 create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat
 
-# Configurable
+
 create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 LayerRouter_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 LayerRouter_0_Address
 create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut_Layer_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_Layer_0
 
+
 create_bd_cell -type ip -vlnv xilinx.com:ip:Neuron_wrapper:1.0 Neuron_0_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 Neuron_0_0_Address
 
+
 create_bd_cell -type ip -vlnv xilinx.com:ip:Neuron_wrapper:1.0 Neuron_0_1
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 Neuron_0_1_Address
+
 
 create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:4.0 LayerRouter_1
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 LayerRouter_1_Address
 create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut_Layer_1
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat_Layer_1
 
+
 create_bd_cell -type ip -vlnv xilinx.com:ip:Neuron_wrapper:1.0 Neuron_1_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 Neuron_1_0_Address
+
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:Neuron_wrapper:1.0 Neuron_1_1
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 Neuron_1_1_Address
 
+
 create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 StimulusRegister_0
+
+
 create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 StimulusRegister_1
 
-##### Configure components #####
+
 set_property -dict [list CONFIG.packetOutWidth {31} CONFIG.packetInWidth {32}] [get_bd_cells TypeRouter_Stimulus]
 set_property -dict [list CONFIG.CONST_VAL {1}] [get_bd_cells TypeRouter_Stimulus_Address]
 set_property -dict [list CONFIG.packetOutWidth {31} CONFIG.packetInWidth {32}] [get_bd_cells TypeRouter_Weight]
@@ -59,27 +68,35 @@ set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_Layers]
 set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_StimulusRegisters]
 set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut]
 
-# Configurable
+
 set_property -dict [list CONFIG.packetOutWidth {26} CONFIG.packetInWidth {31}] [get_bd_cells LayerRouter_0]
-set_property -dict [list CONFIG.CONST_WIDTH {5} CONFIG.CONST_VAL {1}] [get_bd_cells LayerRouter_0_Address]
+set_property -dict [list CONFIG.CONST_WIDTH {5} CONFIG.CONST_VAL {0}] [get_bd_cells LayerRouter_0_Address]
 set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_Layer_0]
 
-set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {1}] [get_bd_cells Neuron_0_0_Address]
 
-set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {2}] [get_bd_cells Neuron_0_1_Address]
+set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {0}] [get_bd_cells Neuron_0_0_Address]
+
+
+set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {1}] [get_bd_cells Neuron_0_1_Address]
+
 
 set_property -dict [list CONFIG.packetOutWidth {26} CONFIG.packetInWidth {31}] [get_bd_cells LayerRouter_1]
-set_property -dict [list CONFIG.CONST_WIDTH {5} CONFIG.CONST_VAL {2}] [get_bd_cells LayerRouter_1_Address]
+set_property -dict [list CONFIG.CONST_WIDTH {5} CONFIG.CONST_VAL {1}] [get_bd_cells LayerRouter_1_Address]
 set_property -dict [list CONFIG.n {2}] [get_bd_cells DoneOut_Layer_1]
+
+
+set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {0}] [get_bd_cells Neuron_1_0_Address]
+
 
 set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {1}] [get_bd_cells Neuron_1_1_Address]
 
-set_property -dict [list CONFIG.CONST_WIDTH {6} CONFIG.CONST_VAL {2}] [get_bd_cells Neuron_1_0_Address]
 
-set_property -dict [list CONFIG.address {1} CONFIG.addressWidth {19} CONFIG.dataWidth {13}] [get_bd_cells StimulusRegister_0]
-set_property -dict [list CONFIG.address {2} CONFIG.addressWidth {19} CONFIG.dataWidth {13}] [get_bd_cells StimulusRegister_1]
+set_property -dict [list CONFIG.address {0} CONFIG.addressWidth {19} CONFIG.dataWidth {13}] [get_bd_cells StimulusRegister_0]
 
-##### Connect components #####
+
+set_property -dict [list CONFIG.address {1} CONFIG.addressWidth {19} CONFIG.dataWidth {13}] [get_bd_cells StimulusRegister_1]
+
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins TypeRouter_Weight/CLK]
 connect_bd_net [get_bd_ports PKT_IN_VALID] [get_bd_pins TypeRouter_Weight/PKT_IN_VALID]
 connect_bd_net [get_bd_ports PKT_IN] [get_bd_pins TypeRouter_Weight/PKT_IN]
@@ -99,13 +116,13 @@ connect_bd_net [get_bd_pins DoneOutConcat/dout] [get_bd_pins DoneOut/DIN]
 connect_bd_net [get_bd_pins DoneOutConcat_Layers/dout] [get_bd_pins DoneOut_Layers/DIN]
 connect_bd_net [get_bd_pins DoneOutConcat_StimulusRegisters/dout] [get_bd_pins DoneOut_StimulusRegisters/DIN]
 
-# Configurable
-# Stimulus registers
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins StimulusRegister_0/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins StimulusRegister_0/RST]
 connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT] [get_bd_pins StimulusRegister_0/PKT_IN]
 connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT_VALID] [get_bd_pins StimulusRegister_0/PKT_IN_VALID]
 connect_bd_net [get_bd_pins StimulusRegister_0/DONE_OUT] [get_bd_pins DoneOutConcat_StimulusRegisters/In0]
+
 
 connect_bd_net [get_bd_ports CLK] [get_bd_pins StimulusRegister_1/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins StimulusRegister_1/RST]
@@ -113,8 +130,7 @@ connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT] [get_bd_pins StimulusRe
 connect_bd_net [get_bd_pins TypeRouter_Stimulus/PKT_OUT_VALID] [get_bd_pins StimulusRegister_1/PKT_IN_VALID]
 connect_bd_net [get_bd_pins StimulusRegister_1/DONE_OUT] [get_bd_pins DoneOutConcat_StimulusRegisters/In1]
 
-# For each layer; for each neuron
-# Checked BLOCK
+
 connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT_VALID] [get_bd_pins LayerRouter_0/PKT_IN_VALID]
 connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT] [get_bd_pins LayerRouter_0/PKT_IN]
 connect_bd_net [get_bd_ports CLK] [get_bd_pins LayerRouter_0/CLK]
@@ -123,7 +139,7 @@ connect_bd_net [get_bd_pins DoneOut_Layer_0/DOUT] [get_bd_pins LayerRouter_0/DON
 connect_bd_net [get_bd_pins LayerRouter_0_Address/dout] [get_bd_pins LayerRouter_0/ADDR]
 connect_bd_net [get_bd_pins LayerRouter_0/DONE_OUT] [get_bd_pins DoneOutConcat_Layers/In0]
 
-# Checked BLOCK
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins Neuron_0_0/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins Neuron_0_0/RST]
 connect_bd_net [get_bd_pins Neuron_0_0_Address/dout] [get_bd_pins Neuron_0_0/NEURON_ADDR]
@@ -131,7 +147,7 @@ connect_bd_net [get_bd_pins LayerRouter_0/PKT_OUT] [get_bd_pins Neuron_0_0/PKT_I
 connect_bd_net [get_bd_pins LayerRouter_0/PKT_OUT_VALID] [get_bd_pins Neuron_0_0/PKT_IN_VALID]
 connect_bd_net [get_bd_pins Neuron_0_0/DONE_OUT] [get_bd_pins DoneOutConcat_Layer_0/In0]
 
-# Checked BLOCK
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins Neuron_0_1/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins Neuron_0_1/RST]
 connect_bd_net [get_bd_pins Neuron_0_1_Address/dout] [get_bd_pins Neuron_0_1/NEURON_ADDR]
@@ -139,7 +155,7 @@ connect_bd_net [get_bd_pins LayerRouter_0/PKT_OUT] [get_bd_pins Neuron_0_1/PKT_I
 connect_bd_net [get_bd_pins LayerRouter_0/PKT_OUT_VALID] [get_bd_pins Neuron_0_1/PKT_IN_VALID]
 connect_bd_net [get_bd_pins Neuron_0_1/DONE_OUT] [get_bd_pins DoneOutConcat_Layer_0/In1]
 
-# Checked BLOCK
+
 connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT_VALID] [get_bd_pins LayerRouter_1/PKT_IN_VALID]
 connect_bd_net [get_bd_pins TypeRouter_Weight/PKT_OUT] [get_bd_pins LayerRouter_1/PKT_IN]
 connect_bd_net [get_bd_ports CLK] [get_bd_pins LayerRouter_1/CLK]
@@ -148,47 +164,62 @@ connect_bd_net [get_bd_pins DoneOut_Layer_1/DOUT] [get_bd_pins LayerRouter_1/DON
 connect_bd_net [get_bd_pins LayerRouter_1_Address/dout] [get_bd_pins LayerRouter_1/ADDR]
 connect_bd_net [get_bd_pins LayerRouter_1/DONE_OUT] [get_bd_pins DoneOutConcat_Layers/In1]
 
-# Checked BLOCK
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins Neuron_1_0/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins Neuron_1_0/RST]
-connect_bd_net [get_bd_pins Neuron_1_0_Address/dout] [get_bd_pins Neuron_1_1/NEURON_ADDR]
+connect_bd_net [get_bd_pins Neuron_1_0_Address/dout] [get_bd_pins Neuron_1_0/NEURON_ADDR]
 connect_bd_net [get_bd_pins LayerRouter_1/PKT_OUT] [get_bd_pins Neuron_1_0/PKT_IN]
 connect_bd_net [get_bd_pins LayerRouter_1/PKT_OUT_VALID] [get_bd_pins Neuron_1_0/PKT_IN_VALID]
 connect_bd_net [get_bd_pins Neuron_1_0/DONE_OUT] [get_bd_pins DoneOutConcat_Layer_1/In0]
 
-# Checked BLOCK
+
 connect_bd_net [get_bd_ports CLK] [get_bd_pins Neuron_1_1/CLK]
 connect_bd_net [get_bd_ports RST] [get_bd_pins Neuron_1_1/RST]
-connect_bd_net [get_bd_pins Neuron_1_1_Address/dout] [get_bd_pins Neuron_1_0/NEURON_ADDR]
+connect_bd_net [get_bd_pins Neuron_1_1_Address/dout] [get_bd_pins Neuron_1_1/NEURON_ADDR]
 connect_bd_net [get_bd_pins LayerRouter_1/PKT_OUT] [get_bd_pins Neuron_1_1/PKT_IN]
 connect_bd_net [get_bd_pins LayerRouter_1/PKT_OUT_VALID] [get_bd_pins Neuron_1_1/PKT_IN_VALID]
 connect_bd_net [get_bd_pins Neuron_1_1/DONE_OUT] [get_bd_pins DoneOutConcat_Layer_1/In1]
 
 
-# Network input
 connect_bd_net [get_bd_pins StimulusRegister_0/VAL_OUT] [get_bd_pins Neuron_0_0/SYN_0_DIN]
 connect_bd_net [get_bd_pins StimulusRegister_0/VAL_OUT_VALID] [get_bd_pins Neuron_0_0/SYN_0_VALID]
+
+
 connect_bd_net [get_bd_pins StimulusRegister_0/VAL_OUT] [get_bd_pins Neuron_0_1/SYN_0_DIN]
 connect_bd_net [get_bd_pins StimulusRegister_0/VAL_OUT_VALID] [get_bd_pins Neuron_0_1/SYN_0_VALID]
+
+
 connect_bd_net [get_bd_pins StimulusRegister_1/VAL_OUT] [get_bd_pins Neuron_0_0/SYN_1_DIN]
 connect_bd_net [get_bd_pins StimulusRegister_1/VAL_OUT_VALID] [get_bd_pins Neuron_0_0/SYN_1_VALID]
+
+
 connect_bd_net [get_bd_pins StimulusRegister_1/VAL_OUT] [get_bd_pins Neuron_0_1/SYN_1_DIN]
 connect_bd_net [get_bd_pins StimulusRegister_1/VAL_OUT_VALID] [get_bd_pins Neuron_0_1/SYN_1_VALID]
 
-# Interconnects
+
 connect_bd_net [get_bd_pins Neuron_0_0/SYN_OUT] [get_bd_pins Neuron_1_0/SYN_0_DIN]
 connect_bd_net [get_bd_pins Neuron_0_0/SYN_OUT_VALID] [get_bd_pins Neuron_1_0/SYN_0_VALID]
+
+
 connect_bd_net [get_bd_pins Neuron_0_0/SYN_OUT] [get_bd_pins Neuron_1_1/SYN_0_DIN]
 connect_bd_net [get_bd_pins Neuron_0_0/SYN_OUT_VALID] [get_bd_pins Neuron_1_1/SYN_0_VALID]
+
+
 connect_bd_net [get_bd_pins Neuron_0_1/SYN_OUT] [get_bd_pins Neuron_1_0/SYN_1_DIN]
 connect_bd_net [get_bd_pins Neuron_0_1/SYN_OUT_VALID] [get_bd_pins Neuron_1_0/SYN_1_VALID]
+
+
 connect_bd_net [get_bd_pins Neuron_0_1/SYN_OUT] [get_bd_pins Neuron_1_1/SYN_1_DIN]
 connect_bd_net [get_bd_pins Neuron_0_1/SYN_OUT_VALID] [get_bd_pins Neuron_1_1/SYN_1_VALID]
 
-# Network output
+
 connect_bd_net [get_bd_pins Neuron_1_0/SYN_OUT] [get_bd_ports SYN_0_OUT]
 connect_bd_net [get_bd_pins Neuron_1_0/SYN_OUT_VALID] [get_bd_ports SYN_0_OUT_VALID]
+
+
 connect_bd_net [get_bd_pins Neuron_1_1/SYN_OUT] [get_bd_ports SYN_1_OUT]
 connect_bd_net [get_bd_pins Neuron_1_1/SYN_OUT_VALID] [get_bd_ports SYN_1_OUT_VALID]
 
+
 regenerate_bd_layout
+
