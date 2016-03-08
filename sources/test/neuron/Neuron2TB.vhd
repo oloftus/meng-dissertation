56dab06a1488330f1166669d5cde82db208a8350
Oliver Loftus <o@oloft.us>
2016-03-08 14:26:00 +0000

    Added further test case to Neuron2TB

--- sources/test/neuron/Neuron2TB.vhd
+++ sources/test/neuron/Neuron2TB.vhd
@@ -23,7 +23,7 @@ architecture Behavioral of Neuron2TB is
        );
         end component Neuron;
         
-    signal sigClk, sigRst, sigPktInvalid, sigSyn0Valid, sigSyn1Valid, sigSynOutValid, sigDoneOut : STD_LOGIC;
+    signal sigClk, sigRst, sigPktInValid, sigSyn0Valid, sigSyn1Valid, sigSynOutValid, sigDoneOut : STD_LOGIC;
     signal sigPktIn : STD_LOGIC_VECTOR (25 downto 0);
     signal sigSyn0Din, sigSyn1Din : STD_LOGIC_VECTOR (15 downto 0);
     signal sigSynOut :  STD_LOGIC_VECTOR (15 downto 0);
@@ -65,26 +65,55 @@ begin
         sigPktInValid <= '0';
         
         sigPktInValid <= '1';
-        sigPktIn <= neuronAddr & "0000" & "0" & "00000001" & "1000000"; -- 1.5
+        sigPktIn <= neuronAddr & "0000" & "0" & "00000000" & "1000000"; -- 0.5
         wait for 200ns;
         sigPktInValid <= '0';
 
         wait until sigDoneOut = '1';
         wait until sigDoneOut = '0';
-        wait for 200ns;
+        wait for 100ns;
 
         sigPktInValid <= '1';
-        sigPktIn <= neuronAddr & "0001" & "0" & "00000000" & "1000000"; -- 0.5
+        sigPktIn <= neuronAddr & "0001" & "0" & "00000000" & "0100000"; -- 0.25
         wait for 200ns;
         sigPktInValid <= '0';
 
-        sigSyn0Din <= "0"&"00000010"&"1000000"; -- 2.5
+        wait until sigDoneOut = '1';
+        wait until sigDoneOut = '0';
+        wait for 100ns;
+
+        -- Test the neuron works
+        sigSyn0Din <= "0"&"00000011"&"0000000"; -- 3
         sigSyn0Valid <= '1';
-        sigSyn1Din <= "0"&"00000011"&"1000000"; -- 3.5
-        sigSyn1Valid <= '1';        
         wait for 200ns;
         sigSyn0Valid <= '0';
+        
+        sigSyn1Din <= "0"&"00000001"&"0000000"; -- 1
+        sigSyn1Valid <= '1';        
+        wait for 200ns;
         sigSyn1Valid <= '0';
+
+        wait until sigSynOutValid = '1';
+        wait for 10ns;
+        assert sigSynOut = "0" & "00000000" & "1101100" report "Test failed: 1"; -- 0.84375
+        
+        wait until sigSynOutValid = '0';
+        wait for 100ns;
+
+        -- Test the neuron works again
+        sigSyn0Din <= "0"&"00000110"&"0000000"; -- 6
+        sigSyn0Valid <= '1';
+        wait for 200ns;
+        sigSyn0Valid <= '0';
+        
+        sigSyn1Din <= "0"&"00000010"&"0000000"; -- 2
+        sigSyn1Valid <= '1';        
+        wait for 200ns;
+        sigSyn1Valid <= '0';
+
+        wait until sigSynOutValid = '1';
+        wait for 10ns;
+        assert sigSynOut = "0" & "00000000" & "1111010" report "Test failed: 2"; -- 0.953125
         
         wait;
     end process;
