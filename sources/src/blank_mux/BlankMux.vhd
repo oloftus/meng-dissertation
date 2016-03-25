b93081ff55cf7be589841f0772a85b3653417c8e
Oliver Loftus <o@oloft.us>
2016-03-25 12:55:20 +0000

    Fixed BlankMux component

--- sources/src/blank_mux/BlankMux.vhd
+++ sources/src/blank_mux/BlankMux.vhd
@@ -6,13 +6,13 @@ entity BlankMux is
         width : INTEGER
     );
     port (
-        SEL : STD_LOGIC;
-        DIN : STD_LOGIC_VECTOR (width - 1 downto 0);
-        DOUT : STD_LOGIC_VECTOR (width - 1 downto 0)
+        SEL : in STD_LOGIC;
+        DIN : in STD_LOGIC_VECTOR (width - 1 downto 0);
+        DOUT : out STD_LOGIC_VECTOR (width - 1 downto 0)
     );
 end BlankMux;
 
 architecture Behavioral of BlankMux is
 begin
-    DOUT <= DIN when SEL = 1 else (others => '0');
+    DOUT <= DIN when SEL = '1' else (others => '0');
 end Behavioral;
