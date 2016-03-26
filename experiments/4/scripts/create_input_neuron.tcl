758891199e2a1c45970ed87b8656a0d3f241278d
Oliver Loftus <o@oloft.us>
2016-03-26 00:34:57 +0000

    Updated Zynq software to support experiment 4 Added weights (1.4.7) for support experiment 4 Added archive information for experiment 4 minus the reports

---
+++ experiments/4/scripts/create_input_neuron.tcl
@@ -0,0 +1,220 @@
+
+create_bd_port -dir I -from 17 -to 0 -type data PKT_IN
+create_bd_port -dir I PKT_IN_VALID
+create_bd_port -dir I -from 2 -to 0 -type data NEURON_ADDR
+create_bd_port -dir O DONE_OUT
+
+
+create_bd_port -dir I -type clk CLK
+create_bd_port -dir I -type rst RST
+create_bd_port -dir O  -type data SYN_OUT
+create_bd_port -dir O SYN_OUT_VALID
+
+
+create_bd_port -dir I SYN_0_VALID
+create_bd_port -dir I -from 0 -to 0 -type data SYN_0_DIN
+
+
+create_bd_port -dir I SYN_1_VALID
+create_bd_port -dir I -from 0 -to 0 -type data SYN_1_DIN
+
+
+create_bd_port -dir I SYN_2_VALID
+create_bd_port -dir I -from 0 -to 0 -type data SYN_2_DIN
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:ValueRouter:1.0 NeuronRouter
+create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 BiasRegister
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 DoneOut
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 DoneOutConcat
+
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 PlanSlicer_Int
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 PlanSlicer_FracHalf
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 PlanSlicerIntFracConcat
+create_bd_cell -type ip -vlnv oloftus.com:prif:OrN:1.0 RoundUp
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:AndN:1.0 MultiplierEnable
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 MultiplierEnableConcat
+create_bd_cell -type ip -vlnv oloftus.com:prif:SumJunction:1.0 SumJunction
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 SumJunctionConcat
+create_bd_cell -type ip -vlnv oloftus.com:prif:ValidSetter:1.0 ValidSetter
+create_bd_cell -type ip -vlnv oloftus.com:prif:Plan:1.0 Plan
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 WeightRegister_0
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:BlankMux:1.0 WeightMux_0
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:Synapse:1.0 Synapse_0
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 WeightRegister_1
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:BlankMux:1.0 WeightMux_1
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:Synapse:1.0 Synapse_1
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:AddressableRegister:1.0 WeightRegister_2
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:BlankMux:1.0 WeightMux_2
+
+
+create_bd_cell -type ip -vlnv oloftus.com:prif:Synapse:1.0 Synapse_2
+
+
+set_property -dict [list CONFIG.packetInWidth {18} CONFIG.packetOutWidth {15}] [get_bd_cells NeuronRouter]
+set_property -dict [list CONFIG.n {4}] [get_bd_cells DoneOut]
+set_property -dict [list CONFIG.NUM_PORTS {4}] [get_bd_cells DoneOutConcat]
+set_property -dict [list CONFIG.address {0} CONFIG.addressWidth {3} CONFIG.dataWidth {12} CONFIG.padHighWidth {0} CONFIG.padLowWidth {0}] [get_bd_cells BiasRegister]
+
+
+set_property -dict [list CONFIG.DIN_WIDTH {12} CONFIG.DIN_TO {6} CONFIG.DIN_FROM {6}] [get_bd_cells PlanSlicer_FracHalf]
+set_property -dict [list CONFIG.DIN_WIDTH {12} CONFIG.DIN_TO {7} CONFIG.DIN_FROM {7}] [get_bd_cells PlanSlicer_Int]
+set_property -dict [list CONFIG.n {2}] [get_bd_cells RoundUp]
+
+
+set_property -dict [list CONFIG.latency {5}] [get_bd_cells ValidSetter]
+set_property -dict [list CONFIG.inputWidth {12} CONFIG.numInputs {4}] [get_bd_cells SumJunction]
+set_property -dict [list CONFIG.n {3}] [get_bd_cells MultiplierEnable]
+set_property -dict [list CONFIG.NUM_PORTS {3}] [get_bd_cells MultiplierEnableConcat]
+set_property -dict [list CONFIG.NUM_PORTS {4} CONFIG.IN0_WIDTH {12} CONFIG.IN1_WIDTH {12} CONFIG.IN2_WIDTH {12} CONFIG.IN3_WIDTH {12}] [get_bd_cells SumJunctionConcat]
+set_property -dict [list CONFIG.integerPrecision {4} CONFIG.fractionPrecision {7}] [get_bd_cells Plan]
+
+
+set_property -dict [list CONFIG.address {1} CONFIG.addressWidth {3} CONFIG.dataWidth {12}] [get_bd_cells WeightRegister_0]
+
+
+set_property -dict [list CONFIG.width {12}] [get_bd_cells WeightMux_0]
+
+
+set_property -dict [list CONFIG.size {1}] [get_bd_cells Synapse_0]
+
+
+set_property -dict [list CONFIG.address {2} CONFIG.addressWidth {3} CONFIG.dataWidth {12}] [get_bd_cells WeightRegister_1]
+
+
+set_property -dict [list CONFIG.width {12}] [get_bd_cells WeightMux_1]
+
+
+set_property -dict [list CONFIG.size {1}] [get_bd_cells Synapse_1]
+
+
+set_property -dict [list CONFIG.address {3} CONFIG.addressWidth {3} CONFIG.dataWidth {12}] [get_bd_cells WeightRegister_2]
+
+
+set_property -dict [list CONFIG.width {12}] [get_bd_cells WeightMux_2]
+
+
+set_property -dict [list CONFIG.size {1}] [get_bd_cells Synapse_2]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins NeuronRouter/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins NeuronRouter/RST]
+connect_bd_net [get_bd_pins DoneOutConcat/dout] [get_bd_pins DoneOut/DIN]
+connect_bd_net [get_bd_pins DoneOut/DOUT] [get_bd_pins NeuronRouter/DONE_IN]
+connect_bd_net [get_bd_ports DONE_OUT] [get_bd_pins NeuronRouter/DONE_OUT]
+connect_bd_net [get_bd_ports PKT_IN_VALID] [get_bd_pins NeuronRouter/PKT_IN_VALID]
+connect_bd_net [get_bd_ports PKT_IN] [get_bd_pins NeuronRouter/PKT_IN]
+connect_bd_net [get_bd_ports NEURON_ADDR] [get_bd_pins NeuronRouter/ADDR]
+connect_bd_net [get_bd_ports CLK] [get_bd_pins BiasRegister/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins BiasRegister/RST]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT] [get_bd_pins BiasRegister/PKT_IN]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT_VALID] [get_bd_pins BiasRegister/PKT_IN_VALID]
+connect_bd_net [get_bd_pins BiasRegister/VAL_OUT] [get_bd_pins SumJunctionConcat/In0]
+connect_bd_net [get_bd_pins BiasRegister/DONE_OUT] [get_bd_pins DoneOutConcat/In0]
+
+
+connect_bd_net [get_bd_pins Plan/Y] [get_bd_pins PlanSlicer_Int/Din]
+connect_bd_net [get_bd_pins Plan/Y] [get_bd_pins PlanSlicer_FracHalf/Din]
+connect_bd_net [get_bd_pins PlanSlicer_Int/Dout] [get_bd_pins PlanSlicerIntFracConcat/In0]
+connect_bd_net [get_bd_pins PlanSlicer_FracHalf/Dout] [get_bd_pins PlanSlicerIntFracConcat/In1]
+connect_bd_net [get_bd_pins PlanSlicerIntFracConcat/dout] [get_bd_pins RoundUp/DIN]
+connect_bd_net [get_bd_pins RoundUp/DOUT] [get_bd_ports SYN_OUT]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins ValidSetter/CLK]
+connect_bd_net [get_bd_ports CLK] [get_bd_pins SumJunction/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins ValidSetter/RST]
+connect_bd_net [get_bd_pins MultiplierEnable/DOUT] [get_bd_pins ValidSetter/SYN_IN_VALID]
+connect_bd_net [get_bd_pins MultiplierEnableConcat/dout] [get_bd_pins MultiplierEnable/DIN]
+connect_bd_net [get_bd_pins SumJunctionConcat/dout] [get_bd_pins SumJunction/DIN]
+connect_bd_net [get_bd_ports SYN_OUT_VALID] [get_bd_pins ValidSetter/SYN_OUT_VALID]
+connect_bd_net [get_bd_pins SumJunction/DOUT] [get_bd_pins Plan/X]
+
+
+connect_bd_net [get_bd_pins WeightRegister_0/VAL_OUT] [get_bd_pins WeightMux_0/DIN]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins WeightRegister_0/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins WeightRegister_0/RST]
+connect_bd_net [get_bd_pins WeightRegister_0/DONE_OUT] [get_bd_pins DoneOutConcat/In1]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT] [get_bd_pins WeightRegister_0/PKT_IN]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT_VALID] [get_bd_pins WeightRegister_0/PKT_IN_VALID]
+
+
+connect_bd_net [get_bd_pins Synapse_0/SYN_OUT] [get_bd_pins WeightMux_0/SEL]
+connect_bd_net [get_bd_pins WeightMux_0/DOUT] [get_bd_pins SumJunctionConcat/In1]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins Synapse_0/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins Synapse_0/RST]
+connect_bd_net [get_bd_pins Synapse_0/SYN_OUT_VALID] [get_bd_pins MultiplierEnableConcat/In0]
+connect_bd_net [get_bd_ports SYN_0_VALID] [get_bd_pins Synapse_0/SYN_IN_VALID]
+connect_bd_net [get_bd_ports SYN_0_DIN] [get_bd_pins Synapse_0/SYN_IN]
+connect_bd_net [get_bd_pins ValidSetter/SYN_IN_CLR] [get_bd_pins Synapse_0/CLR]
+
+
+connect_bd_net [get_bd_pins WeightRegister_1/VAL_OUT] [get_bd_pins WeightMux_1/DIN]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins WeightRegister_1/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins WeightRegister_1/RST]
+connect_bd_net [get_bd_pins WeightRegister_1/DONE_OUT] [get_bd_pins DoneOutConcat/In2]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT] [get_bd_pins WeightRegister_1/PKT_IN]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT_VALID] [get_bd_pins WeightRegister_1/PKT_IN_VALID]
+
+
+connect_bd_net [get_bd_pins Synapse_1/SYN_OUT] [get_bd_pins WeightMux_1/SEL]
+connect_bd_net [get_bd_pins WeightMux_1/DOUT] [get_bd_pins SumJunctionConcat/In2]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins Synapse_1/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins Synapse_1/RST]
+connect_bd_net [get_bd_pins Synapse_1/SYN_OUT_VALID] [get_bd_pins MultiplierEnableConcat/In1]
+connect_bd_net [get_bd_ports SYN_1_VALID] [get_bd_pins Synapse_1/SYN_IN_VALID]
+connect_bd_net [get_bd_ports SYN_1_DIN] [get_bd_pins Synapse_1/SYN_IN]
+connect_bd_net [get_bd_pins ValidSetter/SYN_IN_CLR] [get_bd_pins Synapse_1/CLR]
+
+
+connect_bd_net [get_bd_pins WeightRegister_2/VAL_OUT] [get_bd_pins WeightMux_2/DIN]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins WeightRegister_2/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins WeightRegister_2/RST]
+connect_bd_net [get_bd_pins WeightRegister_2/DONE_OUT] [get_bd_pins DoneOutConcat/In3]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT] [get_bd_pins WeightRegister_2/PKT_IN]
+connect_bd_net [get_bd_pins NeuronRouter/PKT_OUT_VALID] [get_bd_pins WeightRegister_2/PKT_IN_VALID]
+
+
+connect_bd_net [get_bd_pins Synapse_2/SYN_OUT] [get_bd_pins WeightMux_2/SEL]
+connect_bd_net [get_bd_pins WeightMux_2/DOUT] [get_bd_pins SumJunctionConcat/In3]
+
+
+connect_bd_net [get_bd_ports CLK] [get_bd_pins Synapse_2/CLK]
+connect_bd_net [get_bd_ports RST] [get_bd_pins Synapse_2/RST]
+connect_bd_net [get_bd_pins Synapse_2/SYN_OUT_VALID] [get_bd_pins MultiplierEnableConcat/In2]
+connect_bd_net [get_bd_ports SYN_2_VALID] [get_bd_pins Synapse_2/SYN_IN_VALID]
+connect_bd_net [get_bd_ports SYN_2_DIN] [get_bd_pins Synapse_2/SYN_IN]
+connect_bd_net [get_bd_pins ValidSetter/SYN_IN_CLR] [get_bd_pins Synapse_2/CLR]
+
+
+regenerate_bd_layout
+
---
+++ experiments/4/scripts/params.pl
@@ -0,0 +1,48 @@
+use List::Util qw[max];
+
+$HAS_SWRN = 1; # Create the SWRN?
+$USE_MULTS = 1; # LUTs or MULTs for multipliers
+$CCB = 1;
+
+$NUM_LAYERS = 2;
+$NEURONS_PER_LAYER = 5;
+$NUM_INPUTS = 3;
+
+$VAL_INTEGER_PRECISION = 4;
+$VAL_FRACTION_PRECISION = 7;
+$WEIGHT_INTEGER_PRECISION = 5;
+$WEIGHT_FRACTION_PRECISION = 10;
+
+$PKT_TYPE_ADDR_WIDTH = 1;
+$PKT_LAYER_ADDR_WIDTH = 2;
+$PKT_NEURON_ADDR_WIDTH = 3;
+$PKT_SYNAPSE_ADDR_WIDTH = 3;
+$PKT_STIMULUS_REG_ADDR_WIDTH = 2;
+$STIMULUS_TYPE = 1;
+$WEIGHT_TYPE = 0;
+
+$AXI_BUS_WIDTH = 32;
+
+$transferWidth = 1 + $VAL_FRACTION_PRECISION;
+
+if ($CCB) {
+  $transferWidth = 1;
+  ($WEIGHT_INTEGER_PRECISION, $WEIGHT_FRACTION_PRECISION) = ($VAL_INTEGER_PRECISION, $VAL_FRACTION_PRECISION);
+}
+
+$weightWidth = 1 + $WEIGHT_INTEGER_PRECISION + $WEIGHT_FRACTION_PRECISION; # Sign bit
+$valueWidth = 1 + $VAL_INTEGER_PRECISION + $VAL_FRACTION_PRECISION;
+
+$neuronOutPacketWidth = $PKT_SYNAPSE_ADDR_WIDTH + $weightWidth;
+$layerOutPacketWidth = $PKT_NEURON_ADDR_WIDTH + $neuronOutPacketWidth;
+$weightTypeOutPacketWidth = $PKT_LAYER_ADDR_WIDTH + $layerOutPacketWidth;
+$stimulusTypeOutPacketWidth = $PKT_STIMULUS_REG_ADDR_WIDTH + $transferWidth;
+$largestPacketWidth = $PKT_TYPE_ADDR_WIDTH + max($weightTypeOutPacketWidth, $stimulusTypeOutPacketWidth);
+$pktStimulusTypeAddrWidth = $largestPacketWidth - $stimulusTypeOutPacketWidth;
+$pktWeightTypeAddrWidth = $largestPacketWidth - $weightTypeOutPacketWidth;
+
+if ($largestPacketWidth > $AXI_BUS_WIDTH) {
+    die "Packet parameters exceeed AXI bus width";
+}
+
+1
